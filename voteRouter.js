7d1c5147394438347f454ffc53746eead2237bd4
Croustys <akos.barabas2002@gmail.com>
2021-06-05 12:22:47 +0200

    backend email check

--- voteRouter.js
+++ voteRouter.js
@@ -1,21 +1,24 @@
 const router = require("express").Router();
 const Vote = require("./voteModel.model");
+require("dotenv").config();
 
 router.get("/test", async (req, res) => {
-  res.json({ msg: "yes" });
+  res.json({ msg: "test" });
 });
 
 router.post("/add", async (req, res) => {
   const { vote, email } = req.body;
   const newVote = new Vote({ vote, email });
 
-  // @todo implement backend email check.
-  try {
-    const saveVote = await newVote.save();
-    res.json(saveVote);
-  } catch (e) {
-    res.send(`Error: ${e}`);
+  if (email.endsWith(process.env.ENDING)) {
+    try {
+      const saveVote = await newVote.save();
+      res.json(saveVote);
+    } catch (e) {
+      res.send(`Error: ${e}`);
+    }
   }
+  else res.status(400); // bad request
 });
 
 module.exports = router;
