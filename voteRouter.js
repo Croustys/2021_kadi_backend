a4ba0d26223f04afdaadd9671ef1e9f11508362e
Croustys <akos.barabas2002@gmail.com>
2021-06-06 23:21:45 +0200

    email and vote validation

--- voteRouter.js
+++ voteRouter.js
@@ -2,6 +2,16 @@ const router = require("express").Router();
 const Vote = require("./voteModel.model");
 require("dotenv").config();
 
+const CONTESTANTS = [
+  "Boglári",
+  "Gordon",
+  "Zsófi",
+  "Ádi",
+  "Félix",
+  "Denisz",
+  "Virsi",
+];
+
 router.get("/test", async (req, res) => {
   res.json({ msg: "test" });
 });
@@ -10,15 +20,20 @@ router.post("/add", async (req, res) => {
   const { vote, email } = req.body;
   const newVote = new Vote({ vote, email });
 
-  if (email.endsWith(process.env.ENDING)) {
+  const emailArray = await Vote.find({ email });
+  const notExists = Array.isArray(emailArray) && emailArray.length === 0;
+  const validEmail = notExists && email.endsWith(process.env.ENDING);
+
+  const validVote = CONTESTANTS.includes(vote);
+
+  if (validEmail && validVote) {
     try {
       const saveVote = await newVote.save();
       res.json(saveVote);
     } catch (e) {
       res.send(`Error: ${e}`);
     }
-  }
-  else res.status(400); // bad request
+  } else res.sendStatus(400); // bad request
 });
 
 module.exports = router;
